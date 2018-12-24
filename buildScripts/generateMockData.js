afda08bba8e82ac0914be9990060845b732b500e
Jozzey <jozzey@hotmail.com>
2018-12-24 16:07:18 +0000

    Finished setting up mock API for use in dev

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,28 @@
+/*  This scrript generates mock data foor local development.
+    This way ydon't  have tto point to ann actual  API,
+    but you can enjooy realistic, but randomized data,
+    and rapid page loads due to local, static data.
+
+    Since 0.5.x of schema-faker all external generators (chance, faker, etc.) are not built-in, so you need to register as per
+    the line "jsf.extend('faker', () => require('faker'));"
+*/
+
+/* eslint-disable no-console */
+
+
+
+import jsf from  'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+
+jsf.extend('faker', () => require('faker'));
+const json = JSON.stringify(jsf.generate(schema));
+
+fs.writeFile("./src/api/db.json", json, function (err) {
+  if (err) {
+    return console.log(chalk.red(err));
+  } else {
+    console.log(chalk.green("Mock data generated."));
+  }
+});

2fa450c173608b147fb80300fe2ca484990abf00
Jozzey <jozzey@hotmail.com>
2018-12-31 12:27:04 +0000

    Added ability to toggle mock API

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,10 @@
+// By adding useMockApi=true to the URL the app will switch from using the prod API to the dev one
+// e.g. http://localhost:3000/?useMockApi=true
+
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var useMockApi = url.searchParams.get("useMockApi");
+
+  return useMockApi ? 'http://localhost:3001/' : '/';
 }
---
+++ webpack.config.prod.js
@@ -0,0 +1,63 @@
+import path from 'path';
+import webpack from 'webpack';
+
+// Replaced "module.exports =" with "export default" to fix eslint error "No default export found in module  import/default"
+export default {
+  mode: "production", // "production" | "development" | "none"  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  entry: path.resolve(__dirname, 'src/index'), // string | object | array  // defaults to './src'
+  // Here the application starts executing
+  // and webpack starts bundling
+  output: {
+    // options related to how webpack emits results
+    path: path.resolve(__dirname, 'dist'), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "bundle.js", // string    // the filename template for entry chunks
+    publicPath: "/", // string    // the url to the output directory resolved relative to the HTML page
+  },
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        exclude: "/node_modules/",
+        // these are matching conditions, each accepting a regular expression or string
+        // test and include have the same behavior, both must be matched
+        // exclude must not be matched (takes preference over test and include)
+        // Best practices:
+        // - Use RegExp only in test and for filename matching
+        // - Use arrays of absolute paths in include and exclude
+        // - Try to avoid exclude and prefer include
+        loader: "babel-loader",
+        // the loader which should be applied, it'll be resolved relative to the context
+        // -loader suffix is no longer optional in webpack2 for clarity reasons
+        // see webpack 1 upgrade guide
+        options: {
+          presets: ["@babel/preset-env"]
+        },
+        // options for the loader
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ],
+  },
+  devtool: "source-map", // enum  // enhance debugging by adding meta info for the browser devtools
+  // source-map most detailed at the expense of build speed.
+  target: "web", // enum  // the environment in which the bundle should run
+  // changes chunk loading behavior and available modules
+  plugins: [
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+}
